b7c3694a8b09e9c3eed413de3b7820998bd5b082
Mansour Baitar <mansour@servdeck.com>
2024-04-24 10:51:29 +0200

    Added currency formatter

--- view/shared.go
+++ view/shared.go
@@ -2,8 +2,10 @@ package view
 
 import (
 	"context"
+	"fmt"
 	"github.com/mbaitar/levenue-assignment/types"
 	"strconv"
+	"strings"
 )
 
 func AuthenticatedUser(ctx context.Context) types.AuthenticatedUser {
@@ -16,4 +18,20 @@ func AuthenticatedUser(ctx context.Context) types.AuthenticatedUser {
 
 func String(i int) string {
 	return strconv.Itoa(i)
+}
+
+func FormatCurrency(i float64) string {
+	formatted := fmt.Sprintf("%.2f", i)
+	parts := strings.Split(formatted, ".")
+	var integerPart []string
+	integerStr := parts[0]
+
+	for i, r := range integerStr {
+		if i > 0 && (len(integerStr)-i)%3 == 0 {
+			integerPart = append(integerPart, ",")
+		}
+		integerPart = append(integerPart, string(r))
+	}
+	integerFormatted := strings.Join(integerPart, "")
+	return fmt.Sprintf("€%s.%s", integerFormatted, parts[1])
 }
